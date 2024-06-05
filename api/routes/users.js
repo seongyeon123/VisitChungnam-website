f8817cb5b9f80a3b63f5e9da116173791a4becff
seongyeon123 <tjddus1000@naver.com>
2024-06-05 16:37:17 +0900

    유저 수정

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { registerUser, getUserById, getAllUsers, updateUser, deleteUser, getUserReviews } from '../controllers/user.js';
-import { verifyToken, verifyUser, verifyAdmin } from '../utils/verifyToken.js';
+import { verifyToken, verijsonwebtokenfyUser, verifyAdmin } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
@@ -8,18 +8,18 @@ const router = express.Router();
 router.post('/register', registerUser);
 
 // 특정 사용자 가져오기
-router.get('/:userId', verifyUser, getUserById);
+router.get('/:userId', verijsonwebtokenfyUser, getUserById);
 
 // 모든 사용자 가져오기
 router.get('/', verifyAdmin, getAllUsers);
 
 // 사용자 업데이트
-router.put('/:userId', verifyUser, updateUser);
+router.put('/:userId', verijsonwebtokenfyUser, updateUser);
 
 // 사용자 삭제
-router.delete('/:userId', verifyUser, deleteUser);
+router.delete('/:userId', verijsonwebtokenfyUser, deleteUser);
 
 // 특정 사용자의 리뷰 가져오기
-router.get('/:userId/reviews', verifyUser, getUserReviews);
+router.get('/:userId/reviews', verijsonwebtokenfyUser, getUserReviews);
 
 export default router;
